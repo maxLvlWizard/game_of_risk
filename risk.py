df660b24d79a4dacf479de648f0db3a5ab4fd586
MaxLvlWizard <maxroehm@gmail.com>
2022-10-25 21:34:03 -0400

    added rolls to lists

--- risk.py
+++ risk.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+# Ask for number of attacking/defending troops. 
+
 try:
     defending_troops = int(input("Defending troops: "))
     attacking_troops = int(input("Attacking troops: "))
@@ -8,22 +10,38 @@ except ValueError:
     print("Error: Invalid Input.")
     sys.exit(1)
 
-if attacking_troops >= 3:
-    attack_int1 = random.randint(1,6)
-    attack_int2 = random.randint(1,6)
-    attack_int3 = random.randint(1,6)
-elif attacking_troops == 2:
-    attack_int1 = random.randint(1,6)
-    attack_int2 = random.randint(1,6)
-else: 
-    attack_int1 = random.randint(1,6)
+attackers_roll = []
+defenders_roll = []
 
-if defending_troops >= 2:
-    defense_int1 = random.randint(1,6)
-    defense_int2 = random.randint(1,6)
-else:
-    defense_int1 = random.randint(1,6)
+while defending_troops > 0 and attacking_troops > 0:
+    if attacking_troops >= 3:
+        attack_int1 = random.randint(1,6)
+        attack_int2 = random.randint(1,6)
+        attack_int3 = random.randint(1,6)
+        attackers_roll.append(attack_int1)
+        attackers_roll.append(attack_int2)
+        attackers_roll.append(attack_int3)
+    elif attacking_troops == 2:
+        attack_int1 = random.randint(1,6)
+        attack_int2 = random.randint(1,6)
+        attackers_roll.append(attack_int1)
+        attackers_roll.append(attack_int2)
+    else: 
+        attack_int1 = random.randint(1,6)
+        attackers_roll.append(attack_int1)
 
-print("Attacking Dice: ", attack_int1, attack_int2, attack_int3)
-print("Defending Dice: ", defense_int1, defense_int2)
+    if defending_troops >= 2:
+        defense_int1 = random.randint(1,6)
+        defense_int2 = random.randint(1,6)
+        defenders_roll.append(defense_int1)
+        defenders_roll.append(defense_int2)
+    else:
+        defense_int1 = random.randint(1,6)
+        defenders_roll.append(defense_int1)
+
+    print("Attacker's roll: ", attackers_roll)
+    print("Defender's roll: ", defenders_roll)
+
+    defending_troops -= 100
+
 
